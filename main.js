7bdb5bb6fcbbc517503fc69bb6d5ee5a2d171170
Dasoju Shiva Kumar <skumar2@ivycomptech.com>
2022-07-11 16:37:48 +0530

    added logic in js and css as well

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-import './style.css'
+const inputs = document.querySelectorAll('.controls input')
+console.log(inputs);
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+function handleUpdate() {
+  const suffix  = this.dataset.suffix || '';
+  console.log(this.value);
+  document.documentElement.style.setProperty(`--${this.name}`,this.value+suffix);
+}
+
+inputs.forEach(input=>input.addEventListener('change',handleUpdate));
+inputs.forEach(input=>input.addEventListener('mousemove',handleUpdate));
